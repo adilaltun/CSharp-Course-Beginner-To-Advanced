36ac0622f953d5f9e2b952403d692c94eae2f286
Adil ALTUN <adil.altun06@outlook.com>
2024-07-12 12:25:31 +0300

    Fourth Commit
    Integration frontend

--- FirstWebApiProjectInCSharp/Frontend/HotelProject.WebUI/obj/Debug/net5.0/HotelProject.WebUI.RazorTargetAssemblyInfo.cs
+++ FirstWebApiProjectInCSharp/Frontend/HotelProject.WebUI/obj/Debug/net5.0/HotelProject.WebUI.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelProject.WebUI")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5987081d88ad0744682ff050f5be3a3464a62cd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelProject.WebUI.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
--- FirstWebApiProjectInCSharp/WebApiConsume/HotelProject.DtoLayer/obj/Debug/net5.0/HotelProject.DtoLayer.AssemblyInfo.cs
+++ FirstWebApiProjectInCSharp/WebApiConsume/HotelProject.DtoLayer/obj/Debug/net5.0/HotelProject.DtoLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelProject.DtoLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5987081d88ad0744682ff050f5be3a3464a62cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelProject.DtoLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelProject.DtoLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
